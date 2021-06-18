ae1f1409672e6799fd5f6aa0b8108e4676eb25d0
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-06-18 12:23:28 +0100

    Created class for static stations data and migrated models

--- django_dublin_bus/api/models.py
+++ django_dublin_bus/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BusStationsStatic(models.Model):
+
+    stop_id = models.CharField(max_length=15, primary_key=True) 
+    stop_name = models.CharField(max_length=150) 
+    stop_lat = models.DecimalField(max_digits=17, decimal_places=13) 
+    stop_long = models.DecimalField(max_digits=17, decimal_places=13) 
+
+
