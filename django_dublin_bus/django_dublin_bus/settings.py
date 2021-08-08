70d6c41d3ea93da79eeb8793ef8c9f70529a7c19
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-08-08 14:40:08 +0100

    Integrated models and displaying predicted time alongside google's time in app

--- django_dublin_bus/django_dublin_bus/settings.py
+++ django_dublin_bus/django_dublin_bus/settings.py
@@ -132,4 +132,7 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
+
+
+MODELS_DIR = "/Users/brianryan/Documents/CollegeWork/summer/Dublin-Bus-App-Team9/django_dublin_bus/api/models"
